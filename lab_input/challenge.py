e818dda8b4320e4dbfeaef1c183af4e83bba2c3f
eyecancu <c.desousa33@pm.me>
2023-07-03 18:30:50 +0000

    learning about input function

---
+++ lab_input/challenge.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+def main():
+
+    # input asking the user's name
+    name = input("What is your name?")
+    # display the input back to the user
+    print("Name:" + name)
+
+    # what is the day of the week
+    day = input("what is the day of the week?")
+    # input back to user
+    print("Day:" + day)
+    # print statement back
+    print("Hello, " +name +"!", "Happy " +day +"!", sep=" " )
+
+main()
+ 
