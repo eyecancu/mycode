7efc5f35a1599e618e14feb2a4edc8bfc76bd631
eyecancu <c.desousa33@pm.me>
2023-07-05 22:49:02 +0000

    About complex lists

---
+++ mix-list/challenge20.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# create wordbank list
+wordbank= ['indentation', 'spaces']
+
+
+# create student list
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+
+
+# add interger 4 to wordbank
+wordbank.append(4)
+
+# prompt user to input number 0-20 save as variable
+num = input("Pick a number from 0 through 20!")
+
+num = [input]
+
+input = [choice]
+student_name = tlgstudents[choice]
+
+print( student_name  , "always uses" , wordbank[2] , wordbank[1] , "to indent" )
